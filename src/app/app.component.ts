d45a31eb65a7df427020cbbdc4c477105d5f48f8
srikanth <srikanth.varada@cognizant.com>
2019-11-26 10:05:18 -0800

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HelloWorld';
+  myColor = 'rgb(25,200,89)';
+  myClass = 'PURPLE'
+  changeColor():void{
+    if (this.myClass=='PURPLE'){
+      this.myClass = 'GREEN'
+      this.myColor = 'green';
+    } else {
+      this.myClass = 'PURPLE'
+      this.myColor = 'purple';
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BioComponent } from './bio/bio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
